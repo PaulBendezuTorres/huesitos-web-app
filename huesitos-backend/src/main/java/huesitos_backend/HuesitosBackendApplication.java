ae14dc442c9a98c906812792a4b7213f65855932
Paul <u22322437@utp.edu.pe>
2026-05-20 22:58:54 -0500

    feat: agregar proyecto base de Spring Boot para el backend

---
+++ huesitos-backend/src/main/java/huesitos_backend/HuesitosBackendApplication.java
@@ -0,0 +1,13 @@
+package huesitos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuesitosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuesitosBackendApplication.class, args);
+	}
+
+}
---
+++ huesitos-backend/src/test/java/huesitos_backend/HuesitosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package huesitos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuesitosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
