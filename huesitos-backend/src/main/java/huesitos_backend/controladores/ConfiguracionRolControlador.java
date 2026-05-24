4e187d4a44e2af5a2cba4530062277b6074f6867
Paul <u22322437@utp.edu.pe>
2026-05-23 23:11:38 -0500

    fix(controladores): eliminar importacion de HttpStatus no utilizada en ConfiguracionRolControlador

--- huesitos-backend/src/main/java/huesitos_backend/controladores/ConfiguracionRolControlador.java
+++ huesitos-backend/src/main/java/huesitos_backend/controladores/ConfiguracionRolControlador.java
@@ -4,7 +4,6 @@ import huesitos_backend.entidades.ConfiguracionRol;
 import huesitos_backend.entidades.Rol;
 import huesitos_backend.repositorios.ConfiguracionRolRepositorio;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
